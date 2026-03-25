5634edd498f97b2a637af91af384e23f608995cf
vrwadhujka <107637825+vrzonegames@users.noreply.github.com>
2026-03-24 21:21:16 -0400

    Initial commit

---
+++ Assets/Vitamin/Tools/VMod.cs
@@ -0,0 +1,153 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace Vitamin
+{
+    [AddComponentMenu("Vitamin/Mod Loader")]
+    public class VMod : MonoBehaviour
+    {
+        public Path ModPath;
+
+        public List<ModsPaths> ModPaths;
+
+        public string ModsFilePath;
+
+        public enum Path
+        {
+            LocalLow,
+            Roaming,
+            StreamingAssets
+        }
+
+        [System.Serializable]
+        public class ModsPaths
+        {
+            public string Name;
+
+            public string Path;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            LoadAllPaths();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void LoadAllPaths()
+        {
+            if (ModPath == Path.LocalLow)
+            {
+                LocalLow();
+            }
+            if (ModPath == Path.Roaming)
+            {
+                Roaming();
+            }
+            if (ModPath == Path.StreamingAssets)
+            {
+                StreamingAssets();
+            }
+        }
+
+        private void LocalLow()
+        {
+            string P = Application.persistentDataPath + "/Mods";
+
+            Directory.CreateDirectory(P);
+
+            ModsFilePath = P;
+
+            Debug.Log(P);
+        }
+
+        private void Roaming()
+        {
+            string OP = System.IO.Path.Combine(Application.persistentDataPath, "../", "../", "../Roaming") + "/" + Application.companyName + "/Mods";
+
+            string P;
+
+            P = System.IO.Path.GetFullPath( OP );
+
+            Directory.CreateDirectory(P);
+
+            ModsFilePath = P;
+
+            Debug.Log(P);
+        }
+
+        private void StreamingAssets()
+        {
+            string P = Application.streamingAssetsPath + "/Mods";
+
+            Directory.CreateDirectory(P);
+
+            ModsFilePath = P;
+
+            Debug.Log(P);
+        }
+
+        public void LoadPngToRaw(RawImage image,string ModName)
+        {
+            for(int i = 0; i < ModPaths.Count; i++)
+            {
+                if (ModPaths[i].Name == ModName)
+                {
+                    string P = ModsFilePath + ModPaths[i].Path;
+                    
+                    StartCoroutine(DownloadImageToRaw(P, image));
+                    
+                    Debug.Log(ModsFilePath + ModPaths[i].Path);
+                }
+                
+            }
+        }
+        
+        public void LoadPngToMat(Material mat,string ModName)
+        {
+            for(int i = 0; i < ModPaths.Count; i++)
+            {
+                if (ModPaths[i].Name == ModName)
+                {
+                    string P = ModsFilePath + ModPaths[i].Path;
+                    
+                    StartCoroutine(DownloadImageToMat(P, mat));
+                    
+                    Debug.Log(ModsFilePath + ModPaths[i].Path);
+                }
+                
+            }
+        }
+
+        
+        IEnumerator DownloadImageToRaw(string MediaUrl, RawImage I)
+        {   
+            UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
+            yield return request.SendWebRequest();
+            if(request.isNetworkError || request.isHttpError) 
+                Debug.Log(request.error);
+            else
+                I.texture = ((DownloadHandlerTexture) request.downloadHandler).texture;
+        } 
+        
+        IEnumerator DownloadImageToMat(string MediaUrl, Material I)
+        {   
+            UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
+            yield return request.SendWebRequest();
+            if(request.isNetworkError || request.isHttpError) 
+                Debug.Log(request.error);
+            else
+                I.mainTexture = ((DownloadHandlerTexture) request.downloadHandler).texture;
+        }
+    }
+}
